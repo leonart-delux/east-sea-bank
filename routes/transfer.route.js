4724ab2cc359c3ce2ea11dbd0905d4b15972d88e
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-11-13 00:42:08 +0700

    Adjust route for transfer money

--- routes/transfer.route.js
+++ routes/transfer.route.js
@@ -6,14 +6,41 @@ router.get('/', function (req, res) {
     res.render('vwTransfer/transferDefault');
 });
 
+router.post('/', function (req, res) {
+    req.session.transferInfo = {
+        bank: req.body.bank,
+        accountNumber: req.body.accountNumber,
+    };
+    res.redirect('/logged/transfer/transfer-information');
+});
+
+
 // Transfer routing - step 1
-router.get('/step-1', function (req, res) {
-    res.render('vwTransfer/step-1');
+router.get('/transfer-information', function (req, res) {
+    res.render('vwTransfer/transfer-information');
+});
+router.post('/transfer-information', function (req, res) {
+    req.session.moneyAndContent = {
+        money: req.body.moneyAmount,
+        transactionContent: req.body.transactionContent,
+    };
+    res.redirect('/logged/transfer/transfer-confirm');
 });
 
 // Transfer routing - step 2
-router.get('/step-2', function (req, res) {
-    res.render('vwTransfer/transfer2');
+router.get('/transfer-confirm', function (req, res) {
+    res.render('vwTransfer/transfer-confirm', {
+        // transactionInformation: {
+        //     accountReceivedNumber: req.session.transferInfo.accountNumber,
+        //     bank: req.session.transferInfo.bank,
+        //     money: req.session.moneyAndContent.money,
+        //     content: req.session.moneyAndContent.transactionContent,
+        //     accountSentNumber: '134134341234',
+        //         receiverName:"NULL"
+        //
+        //
+        // }
+    });
 });
 
 // Transfer routing - step 3
