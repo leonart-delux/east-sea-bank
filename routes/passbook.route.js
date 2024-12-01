188fd6cf4ddd2a6ce8f34fa48bd0b3d5165e0192
Bien Xuan Huy <bienxhuy@gmail.com>
2024-12-01 08:08:18 +0700

    passbook layout done

--- routes/passbook.route.js
+++ routes/passbook.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import passbookService from '../service/passbookService.js';
 
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('vwPassbook/passbook');
+    const userPassbooks = passbookService.getAll();
+    res.render('vwPassbook/passbook', {
+        passbooks:  userPassbooks,
+    });
 });
 
 router.get('/inspect', function (req, res) {
---
+++ service/passbookService.js
@@ -0,0 +1,10 @@
+export default {
+    getAll() {
+        return [
+            { date: '10/10/2023', totalMoney: '5,000,000', rate: '3%' },
+            { date: '09/09/2023', totalMoney: '100,000,000', rate: '3%' },
+            { date: '08/08/2023', totalMoney: '10,000,000', rate: '3%' },
+            { date: '07/07/2023', totalMoney: '7,000,000', rate: '3%' },
+        ];
+    },
+};
