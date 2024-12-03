b06d46a90188f8c7554a074b813177931edf8869
Bien Xuan Huy <bienxhuy@gmail.com>
2024-12-03 09:56:23 +0700

    add saving option and saving plan service

---
+++ service/savingPlanService.js
@@ -0,0 +1,10 @@
+export default {
+    getAll() {
+        return [
+            { Ma_Goi: '1', Ten_Loai_Hinh_Tiet_Kiem: 'Không kỳ hạn', Thoi_Gian_Ky_Han: 0, Lai_Suat: 0.005 },
+            { Ma_Goi: '2', Ten_Loai_Hinh_Tiet_Kiem: 'Có kỳ hạn', Thoi_Gian_Ky_Han: 3, Lai_Suat: 0.03 },
+            { Ma_Goi: '3', Ten_Loai_Hinh_Tiet_Kiem: 'Có kỳ hạn', Thoi_Gian_Ky_Han: 6, Lai_Suat: 0.05 },
+            { Ma_Goi: '4', Ten_Loai_Hinh_Tiet_Kiem: 'Có kỳ hạn', Thoi_Gian_Ky_Han: 12, Lai_Suat: 0.07 },
+        ];
+    },
+}
