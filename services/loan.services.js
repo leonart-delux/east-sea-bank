d758f3cb95ba5eaa1cc11a70eb06b2f6ab401ee1
Bien Xuan Huy <bienxhuy@gmail.com>
2024-12-06 16:17:07 +0700

    change path and statistic view

--- services/loan.services.js
+++ services/loan.services.js
@@ -20,7 +20,7 @@ export default{
         return await db('khoan_vay')
             .join('tai_khoan', 'khoan_vay.So_Tai_Khoan', '=', 'tai_khoan.So_Tai_Khoan') // JOIN với bảng tài khoản
             .join('khach_hang', 'tai_khoan.Ma_KH', '=', 'khach_hang.Ma_KH') // JOIN với bảng khách hàng
-            .whereRaw('DATE_ADD(Ngay_Bat_Dau, INTERVAL Ky_Han DAY) = ?', [currentDate]) // Ngày hiện tại = ngày bắt đầu + kỳ hạn
+            .whereRaw('DATE_ADD(Ngay_Bat_Dau, INTERVAL Ky_Han MONTH) = ?', [currentDate]) // Ngày hiện tại = ngày bắt đầu + kỳ hạn
             .whereNull('Ngay_Ket_Thuc') // Ngày kết thúc là null (chưa hoàn thành)
             .select(
                 'khoan_vay.*',
@@ -36,7 +36,7 @@ export default{
         return await db('khoan_vay')
             .join('tai_khoan', 'khoan_vay.So_Tai_Khoan', '=', 'tai_khoan.So_Tai_Khoan') // JOIN với bảng tài khoản
             .join('khach_hang', 'tai_khoan.Ma_KH', '=', 'khach_hang.Ma_KH') // JOIN với bảng khách hàng
-            .whereRaw('DATE_ADD(Ngay_Bat_Dau, INTERVAL Ky_Han DAY) < ?', [currentDate]) // Ngày hiện tại > ngày bắt đầu + kỳ hạn
+            .whereRaw('DATE_ADD(Ngay_Bat_Dau, INTERVAL Ky_Han MONTH) < ?', [currentDate]) // Ngày hiện tại > ngày bắt đầu + kỳ hạn
             .whereNull('Ngay_Ket_Thuc') // Ngày kết thúc là null (chưa hoàn thành)
             .select(
                 'khoan_vay.*',
