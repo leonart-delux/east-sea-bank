14f95ceb8d3658184f51ce4619002f2a8124db1f
Bien Xuan Huy <bienxhuy@gmail.com>
2024-12-06 12:01:57 +0700

    add host

--- main.js
+++ main.js
@@ -161,6 +161,10 @@ app.use('/logged/debts/', isAuth, loanRouter);
 app.use('/logged/debtbook', isAuth, debtRouter);
 
 // Listen on port
-app.listen(3000, function () {
+// Use PORT provided in environment or default to 3000
+const port = process.env.PORT || 3000;
+
+// Listen on `port` and 0.0.0.0
+app.listen(port, "0.0.0.0", function () {
     console.log('Server started on http://localhost:3000');
 });
